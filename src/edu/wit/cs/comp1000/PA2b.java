c48341ed5be8f737bcd4cfc91c36495728464fac
Johanna Hoang <hoangp3@wit.edu 10.220.52.170>
2024-10-07 11:39:41 -0400

    panumbers b

--- src/edu/wit/cs/comp1000/PA2b.java
+++ src/edu/wit/cs/comp1000/PA2b.java
@@ -1,11 +1,40 @@
 package edu.wit.cs.comp1000;
 
-// TODO: document this class
+// Johanna Hoang
+import java.util.Scanner;
+
 public class PA2b {
 
-	// TODO: document this function
 	public static void main(String[] args) {
-		// TODO: write your code here
-	}
+Scanner scanner = new Scanner(System.in);
 
+/** gather input from user
+ * 
+ */
+System.out.print("enter a, b, and c: ");
+double a = scanner.nextDouble();
+double b = scanner.nextDouble();
+double c = scanner.nextDouble();
+
+/** calculate the discriminant
+ * 
+ */
+double discriminant = b * b - 4 * a * c;
+
+/** determine roots
+ * 
+ */
+if (discriminant < 0) {
+	System.out.println("Roots: Imaginary");
+} else if (discriminant == 0) {
+	double root = -b/ (2 * a);
+	System.out.printf("Root: %.2f%n", root);
+} else {
+	double root1 = (-b - Math.sqrt(discriminant)) / (2 * a);
+	double root2 = (-b + Math.sqrt(discriminant)) / (2 * a);
+	
+	System.out.printf("roots: %.2f, %.2f%n", Math.min(root1, root2), Math.max(root1, root2));
 }
+	}
+}
+
